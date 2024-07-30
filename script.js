8b778bde40e94b28d8f5db0668343d2c22a50044
Kps-96 <kpsitmail007@gmail.com>
2024-07-30 22:22:10 +0530

    without responsive and login work completed

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const cards = document.querySelector('.cards');
+    const totalCards = document.querySelectorAll('.card').length;
+    const cardWidth = document.querySelector('.card').offsetWidth;
+
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = totalCards - 1;
+    } else if (currentSlide >= totalCards) {
+        currentSlide = 0;
+    }
+
+    cards.style.transform = `translateX(${-currentSlide * (cardWidth + 20)}px)`;
+}
